852a359d2f75c26c4f194802de84a2e5e0460ea9
mbhewitt <dircery@gmail.com>
2023-02-28 14:55:36 -0800

    add documentation

--- app/alphaadvantage.py
+++ app/alphaadvantage.py
@@ -3,10 +3,14 @@ from statistics import mean
 import logging
 
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
+#TODO: Set logging level on command line or config file
 
+
+# Retrieves and averages the closing price of symbol for previous X days
 def getCloseDataAvg(symbol,days):
     stockData={}
     priceList=[]
+    #TODO: move APIKEY out to command line or config file because it is a credential.
     url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey=2PC8C9EVNUKMJRCK'
 
     try:
@@ -37,4 +41,36 @@ def getCloseDataAvg(symbol,days):
     avgStockPrice=mean(priceList)
     print(f"{days} day average closing {symbol} Price ${avgStockPrice}")
 
-getCloseDataAvg("GOOG",5)
+#TODO: move what symbol number of days to command line
+getCloseDataAvg(symbol="GOOG",days=5)
+
+
+
+# https://www.alphavantage.co/documentation/
+# get APIkey https://www.alphavantage.co/support/#api-key
+# Retrieved JSON structure
+
+"""
+{
+    "Meta Data": {
+        "1. Information": "Daily Time Series with Splits and Dividend Events",
+        "2. Symbol": "GOOG",
+        "3. Last Refreshed": "2023-02-27",
+        "4. Output Size": "Compact",
+        "5. Time Zone": "US/Eastern"
+    },
+    "Time Series (Daily)": {
+        "2023-02-27": {
+            "1. open": "90.09",
+            "2. high": "90.4499",
+            "3. low": "89.61",
+            "4. close": "90.1",
+            "5. adjusted close": "90.1",
+            "6. volume": "22724262",
+            "7. dividend amount": "0.0000",
+            "8. split coefficient": "1.0"
+        },
+        ...
+    }
+}
+"""
